cde24ee892ff7a23f94a2bad75183bc0ca005b72
lshaoqin <shaoqin.lu@gmail.com>
2025-06-30 23:27:53 +0800

    update icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 import { useContext } from "react";
 import { CurrentResourceContext } from "@/components/CurrentResourceProvider";
 import { Button } from "@/components/ui/button";
-import { Home, Search } from "lucide-react";
+import { Home } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export default function Header() {
@@ -19,7 +19,7 @@ export default function Header() {
         <Avatar
           className="w-8 h-8 flex items-center justify-center rounded-full text-lg bg-primary text-primary-foreground font-bold"
         >
-          <AvatarImage src={`https://cdn.rafled.com/anime-icons/images/9ab25c9084ff6f2c9be46f81e74df025f047aaf06d237b9f0c652af1cc5abc43.jpg`} alt={`Wayfinder`} />
+          <AvatarImage src="/photo_5989065684065302872_x.jpg" alt={`Wayfinder`} />
           <AvatarFallback>W</AvatarFallback>
         </Avatar>
         <h1 className="text-lg font-bold text-gray-700">Wayfinder</h1>
@@ -32,13 +32,6 @@ export default function Header() {
           className="h-8 w-8"
         >
           <Home className="h-4 w-4" />
-        </Button>
-        <Button 
-          variant="ghost" 
-          size="icon"
-          className="h-8 w-8"
-        >
-          <Search className="h-4 w-4" />
         </Button>
       </div>
     </header>
